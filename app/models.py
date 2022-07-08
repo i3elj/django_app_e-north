001d092279db49b1e70849c810bb14020fe35b57
i3elj <guisande.bernardo@gmail.com>
2022-07-08 17:02:45 -0300

    add: 'grade_four', 'sum' fields; mod: 'round()' function added to 'calculate_average()'

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=180)
-    born_at = models.DateTimeField('born at', null=True, blank=True)
-    phone_number = models.IntegerField(default=0)
+    born_at = models.DateField('born at', null=True, blank=True)
+    phone_number = models.CharField(null=True, blank=True, max_length=20)
 
     def __str__(self) -> str:
         return self.name
@@ -15,11 +15,31 @@ class Grade(models.Model):
     grade_one: float = models.FloatField(default=0.0)
     grade_two: float = models.FloatField(default=0.0)
     grade_three: float = models.FloatField(default=0.0)
+    grade_four: float = models.FloatField(default=0.0)
+    sum: float = models.FloatField(default=0.0)
     average: float = models.FloatField(default=0.0)
 
     def calculate_average(self) -> float:
-        self.average = (self.grade_one + self.grade_two + self.grade_three) / 3
+        self.average = round((self.grade_one + self.grade_two + self.grade_three + self.grade_four) / 4, 2)
         return self.average
+    
+    
+    def total_sum(self) -> float:
+        self.sum = (self.grade_one + self.grade_two + self.grade_three + self.grade_four)
+        return self.sum
+    
+    
+    def traits(self):
+        traits_list = [
+            self.subject,
+            self.grade_one,
+            self.grade_two,
+            self.grade_three,
+            self.grade_four,
+            self.average
+        ]
+        return traits_list
+    
 
     def __str__(self) -> str:
         traits: str = ""
@@ -28,6 +48,7 @@ class Grade(models.Model):
             self.grade_one,
             self.grade_two,
             self.grade_three,
+            self.grade_four,
             self.average
         ]
         for i in range(len(traits_list)):
