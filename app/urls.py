5144b598fde5c9f268847afc412112347be3f9ab
i3elj <guisande.bernardo@gmail.com>
2022-07-08 17:04:58 -0300

    mod: update_students_grade/update_grade/delete_grade urls

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,8 @@ app_name: str = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('update_students_name/<int:pk>/', views.update_student_name, name='update_students_name'),
-    path('update_students_grades/<int:pk>/', views.update_student_grade, name='update_students_grades'),
-    path('update_grade/<int:pk>/', views.update_grade, name='update_grade'),
-    path('delete_grade/<int:pk>/', views.delete_grade, name='delete_grade'),
+    path('update_students_grade/<int:pk>/', views.update_student_grade, name='update_students_grade'),
+    path('update_grade/<int:pk>/<int:sk>/', views.update_grade, name='update_grade'),
+    path('delete_grade/<int:pk>/<int:sk>/', views.delete_grade, name='delete_grade'),
     path('delete_student/<int:pk>/', views.delete_student, name='delete_student'),
 ]
